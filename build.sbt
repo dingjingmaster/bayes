3fee95a2f488baa03f009576c6ace5ea7b4f0e18
丁敬 <dingjing@live.cn>
2019-04-12 18:20:51 +0800

    数据集解释

--- build.sbt
+++ build.sbt
@@ -1,5 +1,7 @@
 name := "bayes"
-
 version := "0.1"
-
 scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-mllib_2.11" % "2.4.1",
+)
--- src/main/scala/com/dingjingmaster/ml/bayes/spark/Bayes.scala
+++ src/main/scala/com/dingjingmaster/ml/bayes/spark/Bayes.scala
@@ -1,6 +1,49 @@
 package com.dingjingmaster.ml.bayes.spark
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
 
+/**
+  *  数据集信息
+  *  根据人口普查数据预测年收入是否超过5万美元/年。
+  *  第一列：年龄（连续）
+  *  第二列：工作类型（离散）Private、 Self - emp - not - inc、 Self - emp - inc、 Federal - gov、 Local - gov
+  *                          State - gov、 Without - pay、 Never - worked
+  *  第三列：一个州内数据观察代表的人数（连续）
+  *  第四列：学历（离散）Bachelors、 Some-college、 11th、 HS-grad、 Prof-school、 Assoc-acdm、 Assoc-voc
+  *                          9th、 7th-8th、 12th、 Masters、 1st-4th、 10th、 Doctorate、 5th-6th、 Preschool
+  *  第五列：education-num（连续）
+  *  第六列：婚姻状况（离散）Married - civ - spouse、 Divorced、 Never - married、 Separated、 Widowed
+  *                          Married - spouse - absent、 Married - AF - spouse
+  *  第七列：职业（离散）Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty,
+  *                      Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving,
+  *                      Priv-house-serv, Protective-serv, Armed-Forces.
+  *  第八列：家庭关系（离散）Wife、 Own - child、  Husband、  Not - in - family、 Other - relative、  Unmarried
+  *  第九咧：种族（离散）White、  Asian - Pac - Islander、  Amer - Indian - Eskimo、  Other、 Black
+  *  第十咧：性别（离散）Female、 Male.
+  *  十一列：capital-gain（连续）
+  *  十二列：capital-loss（连续）
+  *  十三咧：每周工作时长（连续）
+  *  十四列：祖国（离散）United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc),
+  *                      India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica,
+  *                      Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti,
+  *                      Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador,
+  *                      Trinadad&Tobago, Peru, Hong, Holand-Netherlands.
+  *  十五列：年收入是否超过 50K（离散） >=50K <50k
+  *
+  *  数据集来源： http://archive.ics.uci.edu/ml/datasets/Adult
+  */
 object Bayes {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+                .setAppName("bayes")
+                .setMaster("local")
+    val sc = new SparkContext(conf)
+    val dataRDD = sc.textFile(this.HDFS_PATH + "dataset/adult.data")
 
+
+  }
+
+  private val HDFS_PATH = "hdfs://10.26.26.145:8020/rs/dingjing/"
 }
